20f43e7ea952275ac1b83beaa5b25a4ae27393ea
czynastka <czynastka@gmail.com>
2014-05-28 21:23:54 +0200

    Korzystanie z AccountRepository - wstępnie

--- IDepositRepository/IDepositRepository/Program.cs
+++ IDepositRepository/IDepositRepository/Program.cs
@@ -13,19 +13,23 @@ using log4net;
 using NHibernate;
 using Contracts;
 
-namespace IDepositService {
-    class Program {
+namespace IDepositService
+{
+    class Program
+    {
         private const string myAddress = "net.tcp://localhost:50002/IDepositRepository";
         private static IServiceRepository serviceRepository;
 
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
             // Logger Config
             log4net.Config.XmlConfigurator.Configure();
             Logger.logger.Info("Logger initialized.");
 
             ServiceHost sh = new ServiceHost(typeof(DepositRepository), new Uri[] { new Uri(myAddress) });
 
-            try {
+            try
+            {
                 // Starting service
                 sh.AddServiceEndpoint(typeof(IDepositRepository), new NetTcpBinding(), myAddress);
                 sh.Open();
@@ -61,55 +65,92 @@ namespace IDepositService {
                 Logger.logger.Info("Connection to IServiceRepository shut down.");
             }
 
-            catch (CommunicationException comEx) {
+            catch (CommunicationException comEx)
+            {
                 Console.WriteLine("Communication error: " + comEx.Message);
                 sh.Abort();
                 Console.ReadLine();
             }
         }
 
-        private static void Alive(object sender, EventArgs e) {
+        private static void Alive(object sender, EventArgs e)
+        {
             serviceRepository.isAlive("IDepositRepository");
             Logger.logger.Info("Sent isAlive signal.");
         }
-    }
 
-    public class Logger {
-        internal static readonly ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-    }
 
-    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
-    public class DepositRepository : IDepositRepository {
-        public Guid CreateDeposit(Guid ClientID, Guid AccountID, string DepositType, double InterestRate) {
-            DepositDetails deposit = new DepositDetails();
-            deposit.ClientID = ClientID;
-            deposit.DepositID = Guid.NewGuid();
-            deposit.AccountID = AccountID;
-            deposit.DepositType = DepositType;
-            deposit.InterestRate = InterestRate;
-            deposit.CreationDate = DateTime.Now;
-            return deposit.DepositID;
+        public class Logger
+        {
+            internal static readonly ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         }
 
-        public DepositDetails GetDepositDetails(Guid DepositID) {
-            using (ISession session = NHibernateHelper.OpenSession())
-            {   DepositDetails found = session.QueryOver<DepositDetails>().Where(x => x.DepositID == DepositID).SingleOrDefault();
+        [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+        public class DepositRepository : IDepositRepository
+        {
+            public Guid CreateDeposit(Guid ClientID, Guid AccountID, string DepositType, double InterestRate) {
+                string accountRepositoryAddress = serviceRepository.getServiceAddress("IAccountRepository");
+                ChannelFactory<IAccountRepository> cf = new ChannelFactory<IAccountRepository>(new NetTcpBinding(SecurityMode.None));
+                IAccountRepository accountService = cf.CreateChannel();
+                //check if ClientID exists
+                List<AccountDetails> adlist = accountService.GetAccountsById(ClientID);
+                if (adlist == null)
+                {
+                    Logger.logger.Error("Client does not exist.");
+                    return Guid.Empty;
+                }
+                else
+                {
+                    //check if AccountID exists
+                    Boolean aexists = false;
+                    foreach (AccountDetails ad in adlist)
+                    {
+                        if (ad.AccountNumber = AccountID) //AccountNumber is String and AccountID Guid
+                        {
+                            aexists = true;
+                        }
+                    }
+                    if (!aexists)
+                    {
+                        Logger.logger.Error("Account does not exist or belongs to another client.");
+                        return Guid.Empty;
+                    }
+                    DepositDetails deposit = new DepositDetails();
+                    deposit.ClientID = ClientID;
+                    deposit.DepositID = Guid.NewGuid();
+                    deposit.AccountID = AccountID;
+                    deposit.DepositType = DepositType;
+                    deposit.InterestRate = InterestRate;
+                    deposit.CreationDate = DateTime.Now;
+                    return deposit.DepositID;
+                }
+            }
 
-                if (found != null) {
-                    DepositDetails depo = new DepositDetails();
-                    depo.AccountID = found.AccountID;
-                    depo.ClientID = found.ClientID;
-                    depo.DepositID = found.DepositID;
-                    depo.DepositType = found.DepositType;
-                    depo.CreationDate = found.CreationDate;
-                    depo.InterestRate = found.InterestRate;
-                    Logger.logger.Error("Got details of deposit.");
+            public DepositDetails GetDepositDetails(Guid DepositID)
+            {
+                using (ISession session = NHibernateHelper.OpenSession())
+                {
+                    DepositDetails found = session.QueryOver<DepositDetails>().Where(x => x.DepositID == DepositID).SingleOrDefault();
 
-                    return depo;
+                    if (found != null)
+                    {
+                        DepositDetails depo = new DepositDetails();
+                        depo.AccountID = found.AccountID;
+                        depo.ClientID = found.ClientID;
+                        depo.DepositID = found.DepositID;
+                        depo.DepositType = found.DepositType;
+                        depo.CreationDate = found.CreationDate;
+                        depo.InterestRate = found.InterestRate;
+                        Logger.logger.Error("Got details of deposit.");
 
-                } else {
-                    Logger.logger.Error("Could not get details of deposit.");
-                    return null;
+                        return depo;
+
+                    }
+                    else
+                    {
+                        Logger.logger.Error("Could not get details of deposit.");
+                        return null;
+                    }
                 }
             }
         }
